aba7539509d17f2c1274d6e1b2e5f0efc80bd0d5
Jon Perkins <captainperk81@gmail.com>
2018-11-11 09:23:44 -0500

    Defining the TagHelper Class

---
+++ Cities/Infrastucture/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Cities.Infrastucture.TagHelpers
+{
+    public class ButtonTagHelper : TagHelper
+    {
+        public string BsButtonColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"btn btn-{BsButtonColor}");
+        }
+    }
+}
